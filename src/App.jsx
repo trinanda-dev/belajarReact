1fc174b613ca3933d03a0e88b4fa37df7e14276b
Tri Nanda <2001020024@student.umrah.ac.id>
2024-09-27 11:42:21 +0700

    Conver from HTML Tellwindcss to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
 import './App.css'
+import Footer from './component/layout/navbarFooter/Footer';
+import Navbar from './component/layout/navbarFooter/Navbar';
 import HeroSection from './component/parts/landingPage/HeroSection'
 import Button from './component/ui/Button'
 
 function App() {
-  function SendMassage() {
-    return "Halo Disana"
-    
-  }
-  const textUtama = "Hallo disana";
-  let num1 = 127;
   return (
+    <div className="leading-normal tracking-normal text-white gradient" style={{ fontFamily: "'Source Sans Pro', sans-serif" }}>
+      <Navbar />
+      <HeroSection />
+      <Footer/>
 
-    <>
-    
-    <HeroSection/>
-    <h1>Hello dunia</h1>
-    <h1>{textUtama}</h1>
-    <h1>{num1}</h1>
-    <Button backgroundColor={"yellow"}>Test</Button>
-    <Button backgroundColor={"green"}>Hallo Button</Button>
-    <Button backgroundColor={"purple"}>Text Hello</Button>
-
-
-    <div>{SendMassage()}</div>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
