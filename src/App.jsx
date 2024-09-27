e3dbe1d693a8a7417c245a59abd94566f8bb8aa9
Tri Nanda <2001020024@student.umrah.ac.id>
2024-09-27 08:23:02 +0700

    JSX Lebih dalam

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import HeroSection from './component/parts/landingPage/HeroSection'
 import Button from './component/ui/Button'
 
 function App() {
+  function SendMassage() {
+    return "Halo Disana"
+    
+  }
+  const textUtama = "Hallo disana";
+  let num1 = 127;
   return (
+
     <>
-    <Navbar/>
+    
     <HeroSection/>
     <h1>Hello dunia</h1>
+    <h1>{textUtama}</h1>
+    <h1>{num1}</h1>
     <Button>Test</Button>
+    <div>{SendMassage()}</div>
+    {alert("Ngoding yang bener")}
     </>
   )
 }
